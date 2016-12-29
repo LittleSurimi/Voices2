794fce8e5a3a200608ac8b9d2077994d39bc0893
Solene Trousse <solene.trousse@fnzc.co.nz>
2016-12-29 15:51:10 +1300

    fail : add color with volume

--- src/components/ThreeTest.js
+++ src/components/ThreeTest.js
@@ -8,12 +8,17 @@ require('../lib/OrbitControls');
 const HEIGHT = 1100;
 const WIDTH = 1600;
 var uint8 = new Uint8Array(1024);
+var uint8v2 = new Uint8Array(1024);
 let fftArray = [];
-let mainRay = HEIGHT / 8;
+let ampArray = [];
+let mainRay = HEIGHT / 4;
 let minRay = 1;
 let pX = 0;
 let pY = 0;
 let pZ = 0;
+let ringHue = 0;
+let ringSaturation = 0;
+let ringLightness = 0;
 let pointRay = 0;
 
 
@@ -45,20 +50,44 @@ const createPointsPosition = (positions) => {
     return pointPositionArray;
 }
 
-const createDots = (pointPositionArray)=> {
+const CreateRingsColor = (colors) => {
+  console.log('colors', colors);
+    let ringColorArray = [];
+    for (var i = 0; i < colors.length; i++) {
+        ringHue = colors[i];
+        ringSaturation = colors[i];
+        ringLightness = colors[i];
 
+        ringColorArray.push({
+          'ringHue':ringHue,
+          'ringSaturation':ringSaturation,
+          'ringLightness':ringLightness
+        });
+    }
+    console.log(ringColorArray);
+    return ringColorArray;
+}
+
+const createDots = (pointPositionArray, ringColorArray)=> {
+        // DIFERRENCE BTW .map AND lodash.times();
+        // const a = [1, 2, 3];
+        // const b = a.map((n)=> n * 2); //[2, 4, 6]
+        // const c = lodash.times(a.length, (i) => a[i] * 2);
+
+        const rings = pointPositionArray.length / 2048;
+        const dotCount = pointPositionArray.length;
         // define dots
-        const materials = lodash.times(1, n => new THREE.PointsMaterial({
-            size: n,
-            color: new THREE.Color(Math.random(), Math.random(), Math.random())
-        }));
+        const materials = lodash.times(rings, n => new THREE.PointsMaterial({
+           size: 1,
+           color: new THREE.Color(`hsl(${ringColorArray[n].ringHue}, ${ringColorArray[n].ringSaturation}%, ${ringColorArray[n].ringLightness}%)`)
+         }));
 
 
         // create some random points
-        const geometries = lodash.times(1, () => {
+        const geometries = lodash.times(rings, (n) => {
           const oneGeometry = new THREE.Geometry();
-          for(let i = 0; i < pointPositionArray.length; i++){
-            const pointPosition = pointPositionArray[i];
+          for(let i = 0; i < rings; i++){ // dots in one ring
+            const pointPosition = pointPositionArray[i + 2048 * n];
 
             const x = pointPosition.pX;
             const y = pointPosition.pY;
@@ -69,7 +98,7 @@ const createDots = (pointPositionArray)=> {
             return oneGeometry;
         });
 
-        const dots = lodash.times(1, n => ({
+        const dots = lodash.times(rings, n => ({
             material: materials[n],
             geometry: geometries[n],
         }));
@@ -102,17 +131,23 @@ export default class ThreeTest extends Component {
         //Create an AudioAnalyser, passing in the sound and desired fftSize and stop when sound is not playing
         var analyser = new THREE.AudioAnalyser(sound, 2048);
         let pointPositionArray;
+        let ringColorArray;
         const clearFlag = setInterval(() => {
             if (!sound.isPlaying && playStarted) {
                 console.log('not playing');
                 clearInterval(clearFlag);
                 pointPositionArray = createPointsPosition(fftArray);
-                dots = createDots(pointPositionArray);
+                ringColorArray = CreateRingsColor(ampArray)
+                dots = createDots(pointPositionArray,ringColorArray);
                 this.setState({dots: dots});
                 console.log('setted');
             } else {
                 analyser.analyser.getByteFrequencyData(uint8);
                 fftArray = fftArray.concat(...uint8);
+
+                analyser.analyser.getByteTimeDomainData(uint8v2);
+                console.log(uint8v2[0] - 128);
+                ampArray.push(uint8v2[0] - 128);
             }
         }, 1000 / 60);
 
@@ -138,7 +173,7 @@ export default class ThreeTest extends Component {
             const { width, height, cameraprops, dots } = this.state;
             console.log(dots);
     return (
-      <Renderer background={0x202020} width={width} height={height}>
+      <Renderer background={0xff60ff} width={width} height={height}>
         <Scene width={width} height={height} camera="maincamera" orbitControls={THREE.OrbitControls}>
           <PerspectiveCamera name="maincamera" {...cameraprops} />
           {
